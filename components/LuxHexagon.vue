a9cfb9f739db442f6fbd435c2bbfbf3b7a5fa996
Michel Costa <mikelfcosta@live.com>
2017-05-28 02:33:42 -0300

    Set up Landing Page, set up lux-hexagon, set up backgrounds

---
+++ components/LuxHexagon.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hexagon" :class="{ [type]: true, ['shadow-'+shadow]: true }" :style="{ width: width+'px', height: height+'px' }">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['type', 'shadow', 'width', 'height']
+  }
+</script>
+
+<style lang="scss">
+  .shaded {
+    background: url("/img/hexagon-shaded.svg") no-repeat center;
+  }
+
+  .flat {
+    background: url("/img/hexagon-flat.svg") no-repeat center;
+  }
+
+  .border {
+    background: url("/img/hexagon-border.svg") no-repeat center;
+  }
+
+  .border-gray {
+    background: url("/img/hexagon-border-gray.svg") no-repeat center;
+  }
+
+  .hexagon {
+    position: relative;
+    background-size: contain;
+  }
+
+  .shadow-blurred {
+    filter: drop-shadow( 0 0 20px rgba(0,0,0,0.2) )
+  }
+
+  .shadow-smaller {
+    filter: drop-shadow( 0 0 10px rgba(0,0,0,0.3) )
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,66 @@
 <template>
   <div class="container mid-aligner" style="height: 100vh;">
-
+    <lux-hexagon id="logo" class="mid-aligner" width="500" height="500" type="shaded" shadow="blurred">
+      <img src="/img/luxx-logo.svg" alt="Logo" class="logo">
+    </lux-hexagon>
+    <lux-hexagon id="hexagon-a" class="mid-aligner" width="400" height="400" type="border" shadow="smaller">
+      <h1>clique aqui</h1>
+      <h2>para começarmos</h2>
+    </lux-hexagon>
+    <lux-hexagon id="hexagon-b" width="400" height="400" type="border-gray" shadow="smaller"></lux-hexagon>
   </div>
 </template>
 
 <script>
+  import LuxHexagon from '~components/LuxHexagon.vue'
   export default {
     data () {
       return {}
+    },
+    components: {
+      LuxHexagon
     }
   }
 </script>
 
 <style scoped>
+  .logo {
+    position: absolute;
+    z-index: 100;
+    height: 500px;
+  }
 
+  #logo {
+    z-index: 5;
+  }
+
+  #hexagon-a {
+    position: absolute;
+    right: 50px;
+    bottom: 200px;
+    flex-direction: column;
+    z-index: 3;
+  }
+
+  #hexagon-b {
+    position: absolute;
+    right: 30px;
+    bottom: 170px;
+  }
+
+  h1, h2 {
+    font-family: Gotham, sans-serif;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 22px;
+  }
+
+  h1 {
+    color: white;
+    margin-top: 50px;
+  }
+
+  h2 {
+    color: #D9D9D9;
+  }
 </style>
