6a6c264d8caa6aad0eb1e3ab8beee84d1dcb68c5
Michel Costa <mikelfcosta@live.com>
2017-05-29 22:13:10 -0300

    Finished up LuxHeader and LuxBreadcrumbs

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container" style="height: 100vh;">
-    <lux-header></lux-header>
+    <lux-header :breadcrumbs="['Home', '>', 'Login']"></lux-header>
   </div>
 </template>
 
